b1ed079c389d76591e714a3eee6287e899168791
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-09-24 00:23:16 -0300

    Update app routing

---
+++ src/app/repository/repository-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RepositoryPageComponent } from './components/repository-page/repository-page.component';
+
+const routes: Routes = [
+    {
+        path: "",
+        component: RepositoryPageComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule { }
--- src/app/repository/repository.module.ts
+++ src/app/repository/repository.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RepositoryPageComponent } from './components/repository-page/repository-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RepositoryPageComponent
+  ],
   imports: [
     CommonModule
   ]
