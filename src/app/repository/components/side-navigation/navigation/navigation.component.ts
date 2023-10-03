7e1419ca489ad79846a6f8f0a91cfa9128e89b70
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-10-02 23:08:46 -0300

    Deploy cards

--- src/app/repository/components/side-navigation/navigation/navigation.component.ts
+++ src/app/repository/components/side-navigation/navigation/navigation.component.ts
@@ -9,66 +9,153 @@ import { Card } from 'src/app/core/models/card';
 export class NavigationComponent {
   active = 1;
 
-  basicInfo: { tittle: String, cards: Card[] }[] = [
-    { tittle: "Machine Learning", cards: [ 
+  concetps: { tittle: String, cards: Card[] }[] = [
     {
-      tittle: "What is Machine Learning",
-      minutesRead: 10
-    }, 
-    {
-      tittle: "Machine Learning Map",
-      minutesRead: 10
+      tittle: "Machine Learning", cards: [
+        {
+          tittle: "What is Machine Learning",
+          minutesRead: 10
+        },
+        {
+          tittle: "Machine Learning Map",
+          minutesRead: 10
+        },
+        {
+          tittle: "CRISP-DM",
+          minutesRead: 10
+        },
+        {
+          tittle: "Param-type Algorithms",
+          minutesRead: 10
+        },
+        {
+          tittle: "Supervision in Algorithms",
+          minutesRead: 10
+        }
+      ]
     },
     {
-      tittle: "CRISP-DM",
-      minutesRead: 10
+      tittle: "Data Preparation", cards: [
+        {
+          tittle: "Data Preparation",
+          minutesRead: 10
+        },
+        {
+          tittle: "Data Handling",
+          minutesRead: 10
+        }
+      ]
     },
     {
-      tittle: "Param-type Algorithms",
-      minutesRead: 10
+      tittle: "Modeling", cards: [
+        {
+          tittle: "Modeling",
+          minutesRead: 10
+        },
+        {
+          tittle: "Algorithms",
+          minutesRead: 10
+        },
+        {
+          tittle: "Linear Algorithms",
+          minutesRead: 10
+        }
+      ]
     },
     {
-      tittle: "Supervision in Algorithms",
-      minutesRead: 10
+      tittle: "Performance", cards: [
+        {
+          tittle: "Sources of Errors",
+          minutesRead: 10
+        },
+        {
+          tittle: "Low Performance",
+          minutesRead: 10
+        },
+        {
+          tittle: "Performance test",
+          minutesRead: 10
+        },
+        {
+          tittle: "Underfiting-Overfiting",
+          minutesRead: 10
+        },
+        {
+          tittle: "Gradient Descent",
+          minutesRead: 10
+        }
+      ]
     }
-  ]},
-  { tittle: "Data Preparation", cards: [ 
+  ];
+
+  basics: { tittle: String, cards: Card[] }[] = [
     {
-      tittle: "Data Preparation",
-      minutesRead: 10
+      tittle: "Data Understanding", cards: [
+        {
+          tittle: "Poker Problem",
+          minutesRead: 10,
+          miner: true
+        },
+        {
+          tittle: "Iris Problem",
+          minutesRead: 10,
+          miner: true
+        }]
     },
     {
-      tittle: "Data Handling",
-      minutesRead: 10
+      tittle: "Linear Algorithms", cards: [
+        {
+          tittle: "Linear Regression",
+          minutesRead: 10,
+          excel: true,
+          python: true
+        },
+        {
+          tittle: "Logistic Regression",
+          minutesRead: 10,
+          excel: true,
+          python: true
+        },
+        {
+          tittle: "LDA",
+          minutesRead: 10,
+          excel: true,
+          python: true
+        }
+      ]
     }
-  ]},
-  { tittle: "Modeling", cards: [ 
+  ];
+
+  cases: Card[] = [
     {
-      tittle: "Modeling",
-      minutesRead: 10
+      tittle: "Wine",
+      minutesRead: 10,
+      miner: true,
+      python: true
     },
     {
-      tittle: "Algorithms",
-      minutesRead: 10
+      tittle: "Titanic",
+      minutesRead: 10,
+      miner: true,
+      python: true
     },
     {
-      tittle: "Linear Algorithms",
-      minutesRead: 10
+      tittle: "Banking",
+      minutesRead: 10,
+      miner: true,
+      python: true
+    },
+    {
+      tittle: "Iris",
+      minutesRead: 10,
+      miner: true,
+      python: true
+    },
+    {
+      tittle: "Heart",
+      minutesRead: 10,
+      miner: true,
+      python: true
     }
-  ]},
-  { tittle: "Performance", cards: [ 
-    {
-      tittle: "Sources of Errors",
-      minutesRead: 10
-    },
-    {
-      tittle: "Low Performance",
-      minutesRead: 10
-    },
-    {
-      tittle: "Linear Algorithms",
-      minutesRead: 10
-    }
-  ]}
-  ]
+  ];
 }
