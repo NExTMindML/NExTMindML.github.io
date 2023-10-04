be981afb2b435edf1a2d0deb21aeae1177de05d3
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-10-04 19:26:02 -0300

    deploy for vie home page

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingRoutingModule } from './landing-routing.module';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    HomePageComponent
   ],
   imports: [
     CommonModule,
